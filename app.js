90c44756b2970b6a32df8f94c060f20d5fa74351
Wisney Cardeal <wisneycardeal@gmail.com>
2024-11-08 17:08:08 -0300

    Aula1 - Desafio: hora da prática - alert, variaveis, if

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
-alert('Boas vindas ao jogo do número secreto!');
-let numeroSecreto = 29;
-let chute = prompt('Escolha um número entre 1 e 30');
+// Mostre um alerta com a mensagem "Boas vindas ao nosso site!".
+alert('Boas vindas ao nosso site!');
 
-if (chute == numeroSecreto) {
-    console.log('Isso aí! Você descobriu o número secreto');
-} else {
-     console.log('Não foi dessa vez!');
+// Declare uma variável chamada nome e atribua a ela o valor "Lua".
+let nome = "Lua";
+
+// Crie uma variável chamada idade e atribua a ela o valor 25.
+let idade = 25;
+
+// Defina uma variável numeroDeVendas e atribua a ela o valor 50.
+let numeroDeVendas = 50;
+
+// Defina uma variável saldoDisponivel e atribua a ela o valor 1000.
+let saldoDisponivel = 1000;
+
+// Exiba um alerta com o texto "Erro! Preencha todos os campos"
+alert('Erro! Preencha todos os campos');
+
+// Declare uma variável chamada mensagemDeErro e atribua a ela o valor "Erro! Preencha todos os campos" Agora exiba um alerta com o valor da variável mensagemDeErro.
+let mensagemDeErro = 'Erro! Preencha todos os campos';
+alert(mensagemDeErro);
+
+// Use um prompt para perguntar o nome do usuário e armazená-lo na variável nome.
+nome = prompt('Informe o seu nome:');
+
+// Peça ao usuário para digitar sua idade usando um prompt e armazene-a na variável idade.
+idade = prompt('Informe a sua idade');
+
+// Agora, caso a idade seja maior ou igual que 18, exiba um alerta com a mensagem "Pode tirar a habilitação!".
+if (idade >= 18) {
+    alert('Pode tirar a habilitação!');
 }
